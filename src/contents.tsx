fc28c406732a92f12b7b6260da4d9e5747509b83
Satoshi Kawabata <kwst.andromeda.ma@gmail.com>
2020-10-22 00:24:05 +0900

    Refactor

--- src/contents.tsx
+++ src/contents.tsx
@@ -1,15 +1,37 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { VideoOverlap } from "./components/VideoOverlap";
 import { Message } from "./types";
 
 const app = document.createElement("div");
-app.style.position = "fixed";
-app.style.background = "rgba(255,255,255,0.6)";
-app.style.display = "none";
 
 const Main = () => {
   const [video, setVideo] = useState<HTMLVideoElement>();
+  const [isRecording, setIsRecording] = useState(false);
+  const [videoUrl, setVideoUrl] = useState<string | null>(null);
+  const [rect, setRect] = useState<DOMRect | null>(null);
+
+  const onMessage = (message: Message) => {
+    switch (message.type) {
+      case "detect-video-element":
+        const videos = document.querySelectorAll("video");
+        videos.forEach((video) => {
+          video.removeEventListener("mouseover", onVideoMouseOver);
+          video.addEventListener("mouseover", onVideoMouseOver);
+        });
+        break;
+    }
+  };
+
+  const onVideoMouseOver = (e: MouseEvent) => {
+    if (isRecording) {
+      return;
+    }
+    const videoElm = e.target as HTMLVideoElement;
+    const rect = videoElm.getBoundingClientRect();
+    setVideo(videoElm);
+    setRect(rect);
+  };
+
   useEffect(() => {
     // 送信側 contents -> background
     chrome.runtime.sendMessage({
@@ -26,44 +48,90 @@ const Main = () => {
       onMessage(message);
       return;
     });
-
-    const onMessage = (message: Message) => {
-      switch (message.type) {
-        case "detect-video-element":
-          detectVideoElement();
-          break;
-      }
-    };
-
-    const detectVideoElement = () => {
-      const videos = document.querySelectorAll("video");
-      videos.forEach((video) => {
-        video.removeEventListener("mouseover", onVideoMouseOver);
-        video.addEventListener("mouseover", onVideoMouseOver);
-      });
-    };
-
-    const onVideoMouseOver = (e: MouseEvent) => {
-      const videoElm = e.target as HTMLVideoElement;
-      const rect = videoElm.getBoundingClientRect();
-      app.style.top = `${rect.top}px`;
-      app.style.left = `${rect.left}px`;
-      app.style.width = `${rect.width}px`;
-      app.style.height = `${rect.height}px`;
-      app.style.display = "block";
-      setVideo(videoElm);
-    };
-
-    const onAppMouseOut = (e: MouseEvent) => {
-      app.style.display = "none";
-      setVideo(undefined);
-    };
-
-    app.addEventListener("mouseleave", onAppMouseOut);
   }, []);
 
-  if (video) {
-    return <VideoOverlap video={video} />;
+  if (video && rect) {
+    const onClickRecord = () => {
+      const isPlaying = !!(
+        video.currentTime > 0 &&
+        !video.paused &&
+        !video.ended &&
+        video.readyState > 2
+      );
+
+      const stream = (video as any).captureStream() as MediaStream;
+      const mimeType = "webm";
+      const codec = "VP8";
+      const recorder = new MediaRecorder(stream, {
+        mimeType: `video/${mimeType};codecs=${codec}`,
+      });
+
+      recorder.addEventListener("dataavailable", (e) => {
+        const blob = new Blob([e.data], { type: e.data.type });
+        const url = URL.createObjectURL(blob);
+        video.pause();
+        setVideoUrl(url);
+      });
+
+      if (isPlaying) {
+        recorder.start();
+      } else {
+        video.play();
+        recorder.start();
+      }
+      const finishRecord = () => {
+        recorder.stop();
+        setIsRecording(false);
+      };
+
+      video.removeEventListener("ended", finishRecord);
+      video.removeEventListener("pause", finishRecord);
+      video.addEventListener("ended", finishRecord);
+      video.addEventListener("pause", finishRecord);
+
+      setIsRecording(true);
+    };
+
+    const onClickFinish = () => {
+      video.pause();
+    };
+
+    return (
+      <div
+        style={{
+          position: "fixed",
+          background: "rgba(255,255,255,0.6)",
+          top: `${rect.top}px`,
+          left: `${rect.left}px`,
+          width: `${rect.width}px`,
+          height: `${rect.height}px`,
+        }}
+        onMouseLeave={() => {
+          if (isRecording) {
+            return;
+          }
+          setVideo(undefined);
+        }}
+      >
+        <div>
+          <h3>Record this video on play.</h3>
+          {isRecording ? (
+            <button type="button" onClick={onClickFinish}>
+              Finish
+            </button>
+          ) : (
+            <button type="button" onClick={onClickRecord}>
+              Record
+            </button>
+          )}
+          {videoUrl ? (
+            <a download="test.webbm" href={videoUrl}>
+              download
+            </a>
+          ) : null}
+        </div>
+      </div>
+    );
   } else {
     return null;
   }
