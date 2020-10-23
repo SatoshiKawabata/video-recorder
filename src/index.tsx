d5be7265edf71a5ec92d6c3041a31d45348f9305
Satoshi Kawabata <kwst.andromeda.ma@gmail.com>
2020-10-23 22:28:22 +0900

    Update index.html

--- src/index.tsx
+++ src/index.tsx
@@ -4,67 +4,106 @@ import { useVideoRecorder } from "./recorder/VideoRecorder";
 import { startRecording, stopRecording } from "./recorder/VideoRecorder";
 
 const App = () => {
-  const { state, setState } = useVideoRecorder();
-
+  const [isRecording, setIsRecording] = React.useState(false);
+  const [videoDevices, setVideoDevices] = React.useState<MediaDeviceInfo[]>([]);
+  const [audioDevices, setAudioDevices] = React.useState<MediaDeviceInfo[]>([]);
+  const [selectedVideoDeviceId, setSelectedVideoDeviceId] = React.useState<
+    string
+  >();
+  const [selectedAudioDeviceId, setSelectedAudioDeviceId] = React.useState<
+    string
+  >();
+  React.useEffect(() => {
+    (async () => {
+      const { videoDevices, audioDevices } = await getDevices();
+      setVideoDevices([...videoDevices, ...videoDevices, ...videoDevices]);
+      setAudioDevices(audioDevices);
+      setSelectedVideoDeviceId(videoDevices[0].deviceId);
+      setSelectedAudioDeviceId(audioDevices[0].deviceId);
+    })();
+  }, []);
+  console.log(videoDevices);
   return (
-    <div>
-      <h1>Screen Recorder</h1>
-      <ul>
-        {state.inputDevice.audioDevices.map((mdi) => (
-          <li>{mdi.label}</li>
-        ))}
-      </ul>
-      <ul>
-        {state.inputDevice.videoDevices.map((mdi) => (
-          <li>{JSON.stringify(mdi)}</li>
-        ))}
-      </ul>
-      {state.recordConfig.recordingStatus === "init" ? (
+    <>
+      <select
+        onChange={(e) => {
+          setSelectedVideoDeviceId(
+            videoDevices[e.target.selectedIndex].deviceId
+          );
+        }}
+      >
+        {videoDevices.map((d) => {
+          return (
+            <option
+              key={d.deviceId}
+              value={d.deviceId}
+              selected={d.deviceId === selectedVideoDeviceId}
+            >
+              {d.label}
+            </option>
+          );
+        })}
+      </select>
+      <select
+        onChange={(e) => {
+          setSelectedAudioDeviceId(
+            audioDevices[e.target.selectedIndex].deviceId
+          );
+        }}
+      >
+        {audioDevices.map((d) => {
+          return (
+            <option
+              key={d.deviceId}
+              value={d.deviceId}
+              selected={d.deviceId === selectedAudioDeviceId}
+            >
+              {d.label}
+            </option>
+          );
+        })}
+      </select>
+      {isRecording ? (
         <button
           type="button"
-          onClick={async () => {
-            const newState = await startRecording(
-              state.inputType,
-              state.inputDevice,
-              state.recordConfig,
-              state.exportConfig
-            );
-            console.log("newState", newState, state);
-            setState({
-              ...state,
-              ...newState,
-            });
+          onClick={() => {
+            setIsRecording(false);
           }}
         >
-          start-recording
+          Stop
         </button>
       ) : (
         <button
           type="button"
-          onClick={async () => {
-            const newState = await stopRecording(
-              state.recordConfig,
-              state.exportConfig
-            );
-            setState({
-              ...state,
-              ...newState,
-            });
+          onClick={() => {
+            setIsRecording(true);
           }}
         >
-          stop-recording
+          Record
         </button>
       )}
-      {state.exportConfig.blob ? (
-        <a
-          href={URL.createObjectURL(state.exportConfig.blob)}
-          download={state.exportConfig.name}
-        >
-          Download Video
-        </a>
-      ) : null}
-    </div>
+    </>
   );
 };
 
 ReactDOM.render(<App />, document.querySelector("#app"));
+
+const getDevices = async (): Promise<{
+  videoDevices: MediaDeviceInfo[];
+  audioDevices: MediaDeviceInfo[];
+}> => {
+  const infos = await navigator.mediaDevices.enumerateDevices();
+  const audioDevices: MediaDeviceInfo[] = [];
+  const videoDevices: MediaDeviceInfo[] = [];
+  for (const info of infos) {
+    if (info.kind === "audioinput") {
+      audioDevices.push(info);
+    } else if (info.kind === "videoinput") {
+      videoDevices.push(info);
+    }
+  }
+  return {
+    audioDevices,
+    videoDevices,
+  };
+};
